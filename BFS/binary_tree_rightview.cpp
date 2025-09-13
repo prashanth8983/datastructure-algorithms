fd9d351c25f7e2463cd1c94a757cdd5fb6808777
PRASHANTH KUMAR <56990198+prashanth8983@users.noreply.github.com>
2025-09-13 00:50:23 -0400

    Create binary_tree_rightview.cpp

---
+++ BFS/binary_tree_rightview.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        queue<TreeNode*> q;
+        if(root == NULL)
+            return result;
+
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+            
+            while(size--){
+                
+               TreeNode *curr = q.front();
+                q.pop();
+                if(size == 0)
+                    result.push_back(curr->val);
+                
+                if(curr->left)
+                    q.push(curr->left);
+                if(curr->right)
+                    q.push(curr->right);
+            }
+        }
+
+        return result;
+    }
+};
