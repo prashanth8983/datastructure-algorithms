7ca5c179736ba905d167887999246be2e2e890e3
Prashanth Kumar <prashanth8983@gmail.com>
2026-03-22 10:30:17 -0400

    add leetcode 446 arithmetic slices ii stub

---
+++ leetcode/446.arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=446 lang=cpp
+ *
+ * [446] Arithmetic Slices II - Subsequence
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        return vector<int>{};
+    }
+};
+// @lc code=end
+
